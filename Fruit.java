077806a88bda7c13de1fc756a84e21cb88656596
Aaditya Senthilkumar <aadi192723@gmail.com>
2025-06-29 02:03:35 -0400

    added fruit.java which is a child class

---
+++ Fruit.java
@@ -0,0 +1,8 @@
+public class Fruit extends Item{
+    private String type;
+    public Fruit(String type, String name, int quantity){
+        super(name,quantity);
+        this.type=type;
+    }
+
+}
--- main.java
+++ main.java
@@ -2,9 +2,11 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hello world");
         Inventory inventory = new Inventory();
-        Item item1= new Item("APPLES",20);
-        Item item2= new Item("SWORD",2);
-        inventory.addItem(item1);
+        //Item item1= new Item("APPLES",20);
+        Item item2= new Item("Generic item",2);
+        Fruit fruit = new Fruit("Fuji","Apple",20);
+        // inventory.addItem(item1);
+        inventory.addItem(fruit);
         inventory.addItem(item2);
         inventory.displayInventory();
     }
